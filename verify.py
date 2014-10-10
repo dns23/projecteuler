553d6ce31a9d588bf7a07cf0401cd61a3f9b0062
Dean Sutton <d.n.sutton@gmail.com>
2014-10-10 15:24:37 +1300

    2nd problem solved in Python.

--- verify.py
+++ verify.py
@@ -1,3 +1,5 @@
-from problems.number1.solver import solver
+from problems.number1.solver import solver as solver1
+from problems.number2.solver import solver as solver2
 
-assert 233168 == solver().brute() == solver().smart()
+assert 233168 == solver1().brute() == solver1().smart()
+assert 4613732 == solver2().brute() == solver2().smart()
