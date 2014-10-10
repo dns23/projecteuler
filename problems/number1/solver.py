a0021fdfd74c73ad3b97d91903f79b09a085def6
Dean Sutton <d.n.sutton@gmail.com>
2014-10-10 15:06:54 +1300

    First problem solved in Python.

---
+++ problems/number1/solver.py
@@ -0,0 +1,13 @@
+class solver:
+
+    def brute(self):
+        sum = 0
+        for i in range(0, 1000, 3):
+            sum += i
+        for i in range(0, 1000, 5):
+            if (i % 3 > 0):
+                sum += i
+        return sum
+
+    def smart(self):
+        return self.brute()
---
+++ verify.py
@@ -0,0 +1,3 @@
+from problems.number1.solver import solver
+
+assert 233168 == solver().brute() == solver().smart()
